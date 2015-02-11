5078b779c80a1684776867880955bde3f9a5df54
guest <littlekev34@hotmail.fr>
2015-02-11 12:11:33 +0100

    mouvement vaisseau

--- mindstormgame.cpp
+++ mindstormgame.cpp
@@ -50,13 +50,14 @@ void MindStormGame::draw(QPainter &painter, QRect &rect){
         auto center=_userShip->getCenter();
         painter.drawLine(xSommet,ySommet,xSommet+(xSommet-center.x()),ySommet+(ySommet-center.y()));
         qDebug() << "Shooting...";
-        qDebug() << "x Center :"<< center.x() << "  y Center : " <<center.y();
+        //qDebug() << "x Center :"<< center.x() << "  y Center : " <<center.y();
     }
 
     //Hatch each mines at 5 seconds (100 loops)
     if(loopCounter == 100){
         hatchMines(painter);
         }
+
 }
 
 void MindStormGame::hatchMines(QPainter &painter){
@@ -92,7 +93,7 @@ void MindStormGame::mousePressed( int x, int y){
 void MindStormGame::keyPressed( int key ){
     qDebug() << "KEY PRESSED " << key;
     switch(key) {
-        case Qt::Key_Up: _userShip->accelerate();
+        case Qt::Key_Up: _userShip->incrementSpeed();
             break;
         case Qt::Key_Down: _userShip->slowDown();
             break;
@@ -123,7 +124,8 @@ void MindStormGame::keyReleased( int key ){
 
 
     switch(key) {
-
+    case Qt::Key_Up:
+        break;
     case Qt::Key_Space:_userShip->_isShooting=false;//End of shots
         break;
     }
@@ -164,6 +166,8 @@ void MindStormGame::step(){
             poly.setPoint(j,x+xD,y+yD);
         }
     }
+
+    _userShip->accelerate();
 }
 
 bool MindStormGame::collision(int x, int y)
